0d7c8997fcd2cb345a1a5ff11ca3d644ebc5611c
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-16 23:23:53 +0530

    home page blog list

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+'use client'
+
+import BlogItems from "@/components/BlogItems";
+import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
    <>
    <Header/>
+   <BlogList/>
    </>
   );
 }
