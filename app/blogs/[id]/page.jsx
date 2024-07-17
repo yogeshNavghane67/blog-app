d9d535f7f4fa92ffebbf3f30c128ad436637dbcd
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-17 20:48:19 +0530

    blog post page

---
+++ app/blogs/[id]/page.jsx
@@ -0,0 +1,146 @@
+"use client";
+
+import { assets, blog_data } from "@/Assets/assets";
+import Footer from "@/components/Footer";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const page = ({ params }) => {
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = () => {
+    for (let i = 0; i < blog_data.length; i++) {
+      if (Number(params.id) === blog_data[i].id) {
+        setData(blog_data[i]);
+        console.log(blog_data[i]);
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+  return data ? (
+    <>
+      <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
+        <div className="flex justify-between items-center">
+            <Link href='/'>
+          <Image
+            src={assets.logo}
+            width={180}
+            alt=""
+            className="w-[130px] sm:w-auto"
+          />
+          </Link>
+          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
+            Get started
+            <Image src={assets.arrow} alt="" />
+          </button>
+        </div>
+        <div className="text-center my-24">
+          <h3 className="text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto">
+            {data.title}
+          </h3>
+          <Image
+            className="mx-auto mt-6 border border-white rounded-full"
+            src={data.author_img}
+            width={60}
+            height={60}
+            alt=""
+          />
+          <p className="mt-1 pb-2 text-lg max-w-[740px] mx-auto">
+            {data.author}
+          </p>
+        </div>
+      </div>
+      <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
+        <Image
+          className="border-4 border-white"
+          src={data.image}
+          width={1280}
+          height={720}
+          alt=""
+        />
+        <h1 className="my-8 text-[26px] font-semibold">Introduction:</h1>
+        <p>{data.description}</p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Step 1: self-Reflection and Goal Setting
+        </h3>
+        <p className="my-3">
+          Before your can manage your lifestyle you must a clear that you want
+          to achieve. start by reflecting Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Nisi accusantium quas odio, soluta nulla
+          deleniti dignissimos? Incidunt ipsa atque dolorem tenetur eos tempore
+          placeat iure ab excepturi! Expedita, ipsam quo.
+        </p>
+        <p className="my-3">
+          Before your can manage your lifestyle you must a clear that you want
+          to achieve. start by reflecting Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Nisi accusantium quas odio, soluta nulla
+          deleniti dignissimos? Incidunt ipsa atque dolorem tenetur eos tempore
+          placeat iure ab excepturi! Expedita, ipsam quo.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Step 2: self-Reflection and Goal Setting
+        </h3>
+        <p className="my-3">
+          Before your can manage your lifestyle you must a clear that you want
+          to achieve. start by reflecting Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Nisi accusantium quas odio, soluta nulla
+          deleniti dignissimos? Incidunt ipsa atque dolorem tenetur eos tempore
+          placeat iure ab excepturi! Expedita, ipsam quo.
+        </p>
+        <p className="my-3">
+          Before your can manage your lifestyle you must a clear that you want
+          to achieve. start by reflecting Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Nisi accusantium quas odio, soluta nulla
+          deleniti dignissimos? Incidunt ipsa atque dolorem tenetur eos tempore
+          placeat iure ab excepturi! Expedita, ipsam quo.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">
+          Step 3: self-Reflection and Goal Setting
+        </h3>
+        <p className="my-3">
+          Before your can manage your lifestyle you must a clear that you want
+          to achieve. start by reflecting Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Nisi accusantium quas odio, soluta nulla
+          deleniti dignissimos? Incidunt ipsa atque dolorem tenetur eos tempore
+          placeat iure ab excepturi! Expedita, ipsam quo.
+        </p>
+        <p className="my-3">
+          Before your can manage your lifestyle you must a clear that you want
+          to achieve. start by reflecting Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Nisi accusantium quas odio, soluta nulla
+          deleniti dignissimos? Incidunt ipsa atque dolorem tenetur eos tempore
+          placeat iure ab excepturi! Expedita, ipsam quo.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">
+         Conclusion:
+        </h3>
+        <p className="my-3">
+          Before your can manage your lifestyle you must a clear that you want
+          to achieve. start by reflecting Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Nisi accusantium quas odio, soluta nulla
+          deleniti dignissimos? Incidunt ipsa atque dolorem tenetur eos tempore
+          placeat iure ab excepturi! Expedita, ipsam quo.
+        </p>
+        <div className="my-24">
+            <p className="text-black font-semibold my-4">Share this article on social media</p>
+            <div className="flex">
+                <Image src={assets.facebook_icon} width={50} alt=""/>
+                <Image src={assets.twitter_icon} width={50} alt=""/>
+                <Image src={assets.googleplus_icon} width={50} alt=""/>
+
+            </div>
+        </div>
+      </div>
+      <Footer/>
+    </>
+  ) : (
+    <></>
+  );
+};
+
+export default page;
--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -14,7 +14,7 @@ const BlogList = () => {
         </div>
         <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
             {blog_data.filter((item)=> menu==="All"?true:item.category===menu).map((item,index) => {
-                return <BlogItems key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
+                return <BlogItems key={index} id={item.id} image={item.image} title={item.title} description={item.description} category={item.category}/>
             })}
         </div>
     </div>
