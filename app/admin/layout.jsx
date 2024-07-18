01baf65209613565fa5a73e0e5c355e385eb8d79
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-18 22:35:52 +0530

    admin dashboard

---
+++ app/admin/layout.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "@/components/AdminComponents/Sidebar";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+            <h3 className="font-medium">Admin Panel</h3>
+          </div>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/admin/subscription/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+const page = () => {
+  return <div></div>;
+};
+
+export default page;
