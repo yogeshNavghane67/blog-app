948e74e860d9005617a3f3ad2092b0e6afc0a0b0
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-19 23:19:46 +0530

    email subscription feature

---
+++ app/api/email/route.js
@@ -0,0 +1,17 @@
+import { ConnectDB } from "@/lib/config/db";
+import EmailModel from "@/lib/models/EmailModel";
+import { NextResponse } from "next/server";
+
+const LoadDB = async () => {
+    await ConnectDB();
+}
+LoadDB();
+
+export async function POST(request){
+    const formData = await request.formData();
+    const emailData = {
+        email:`${formData.get('email')}`,
+    }
+    await EmailModel.create(emailData);
+    return NextResponse.json({success:true,msg:"Email Subscribed"})
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,28 @@
 import { assets } from "@/Assets/assets"
+import axios from "axios";
 import Image from "next/image"
+import { useState } from "react"
+import { toast } from "react-toastify";
 
 const Header = () => {
+
+  const [email,setEmail] = useState("");
+
+  const onSubmitHandler = async(e) =>{
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email",email)
+    const response = await axios.post('/api/email',formData);
+    if(response.data.success){
+      toast.success(response.data.msg);
+      setEmail("");
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+
+
   return (
     <div className="py-5 px-5 md:px-12 lg:px-28">
         <div className="flex justify-between items-center">
@@ -11,8 +32,8 @@ const Header = () => {
         <div className="text-center my-8"> 
             <h1 className="text-3xl sm:text-5xl font-medium">latest Blogs</h1>
             <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Get latest blog update here</p>
-            <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
-                <input type="email" placeholder="Enter your email" className="pl-4 outline-none" />
+            <form onSubmit={onSubmitHandler} className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder="Enter your email" className="pl-4 outline-none" />
                 <button type="submit" className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">Subscribe</button>
             </form>
         </div>
