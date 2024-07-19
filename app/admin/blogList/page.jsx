fbd929b8c44c7ac089eb31d3ce107910e11398c2
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-19 20:45:33 +0530

    blog list page for admin

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,7 +1,50 @@
 "use client";
 
+import BlogTableItem from "@/components/AdminComponents/BlogTableItem";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const page = () => {
-  return <div></div>;
+
+  const [blogs,setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const response = await axios.get('/api/blog')
+    setBlogs(response.data.blogs);
+  }
+
+  useEffect(() =>{
+    fetchBlogs()
+  },[])
+ 
+  return <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16">
+    <h1>All Blogs</h1>
+    <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide">
+      <table className="w-full text-sm text-gray-500">
+        <thead className="text-xs text-gray-700 text-left uppercase bg-slate-50">
+          <tr>
+            <th scope="col" className="hidden sm:block px-6 py-3">
+              Author name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Blog Title
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Date
+            </th>
+            <th scope="col" className=" px-2 py-3">
+              Action
+            </th>
+          </tr>
+        </thead>
+          <tbody> 
+            {blogs.map((item,index)=>{
+            return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date}/>
+            })}
+          </tbody>
+      </table>
+    </div>
+  </div>;
 };
 
 export default page;
